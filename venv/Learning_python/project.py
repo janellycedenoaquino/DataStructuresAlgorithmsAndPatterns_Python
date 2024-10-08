d2158f22d6ca4bb2aee22376728809de66d81001
janellycedenoaquino <janellycedenoo@gmail.com>
2024-10-08 10:37:24 -0400

    cards calculation

--- venv/Learning_python/project.py
+++ venv/Learning_python/project.py
@@ -6,7 +6,7 @@ ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 for rank in ranks:
     for suit in suits:
-        cards.append(rank + " of " + suit)
+        cards.append([suit, rank])
 
 
 def shuffle():
@@ -14,14 +14,42 @@ def shuffle():
 
 
 def deal(amount):
-    dealtcards = []
-    while amount != 0:
-        dealtcards.append(cards.pop())
-        amount = amount - 1
-    return dealtcards
+    cards_dealt = []
+    for x in range(amount):
+        cards_dealt.append(cards.pop())
+    return cards_dealt
+
 
 shuffle()
+
+
+def cardsValue(single_rank):
+    # print(rank, "inside cardsValue")
+    if single_rank == "A":
+      value = 11
+    elif single_rank == "J" or single_rank == "Q" or single_rank == "k":
+      value = 10
+    else:
+      value = int(single_rank)
+
+    return value
+
+
+def totalCardsValue(all_cards):
+    total_card_val = 0
+    for card in all_cards:
+        total_card_val += cardsValue(card[1])
+
+    return total_card_val
+
+
+
+
+
+
 yourCards = deal(7)
-print(cards)
+total = totalCardsValue(yourCards)
 print(yourCards)
+print(total)
+
 
