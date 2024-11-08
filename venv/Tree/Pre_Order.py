a1278c7c3a7dd24b9669d452565c2545c3728481
janellycedenoaquino <54867270+janellycedenoaquino@users.noreply.github.com>
2024-11-08 17:12:16 -0500

    binary stuff (#24)

---
+++ venv/Tree/Pre_Order.py
@@ -0,0 +1,42 @@
+"""
+recursive pre-order
+
+pre-order from top to left then right
+
+recursive vs iterative
+"""
+from TreeDSA import BinaryTree
+
+
+def Recursive_Pre_Ord(root):
+    if root is None:
+        return
+
+    print(' ', root.data)
+    Recursive_Pre_Ord(root.left)
+    Recursive_Pre_Ord(root.right)
+
+
+newTree = BinaryTree()
+newTree.createBinaryTree()
+print("from Recursive_Pre_Ord: ", Recursive_Pre_Ord(newTree.root))
+
+
+def Regular_Pre_Ord(root):
+    if root is None:
+        return
+
+    stack = [root]
+    ans = []
+    while stack:
+        currNode = stack.pop()
+        ans.append(currNode.data)
+
+        if currNode.left: stack.append(currNode.left)
+        if currNode.right: stack.append(currNode.right)
+
+    print(ans)
+
+
+print(Regular_Pre_Ord(newTree.root))
+
