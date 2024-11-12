7db482a75ffe323349ece128cb18e162cf660691
janellycedenoaquino <janellycedenoo@gmail.com>
2024-11-11 19:18:11 -0500

    recursion problems

---
+++ venv/Recursion/Recursion_Overall.py
@@ -0,0 +1,44 @@
+"""
+recursion:
+
+is created with a stack
+calling the function from inside itself over and over again
+
+stack overflow can occur
+when you call the stack too many times and have no more space to call it again
+"""
+
+
+def reverse_string(string):
+    if len(string) == 0:
+        return string
+
+    return reverse_string(string[1:]) + string[0]
+
+
+def factorial(num):
+    if num == 1:
+        return 1
+
+    return num * factorial(num-1)
+
+
+def allPositiveEvenNums(num):
+    if num == 2:
+        return num
+
+    if num % 2 == 0: print(num)
+    return allPositiveEvenNums(num-1)
+
+
+def fibonacci(idx):
+    if idx <= 1:
+        return idx
+
+    return fibonacci(idx-1) + fibonacci(idx-2)
+
+
+print(fibonacci(15))
+# print(allPositiveEvenNums(10))
+# print(reverse_string("hello"))
+# print(factorial(7))
